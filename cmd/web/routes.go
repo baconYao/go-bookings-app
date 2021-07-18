3ce959c17e731fb286330094853eb3884df996c8
peiyao <peiyaochang@qnap.com>
2021-07-18 21:25:47 +0800

    Add html templates and corresponding routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,12 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	// 處理靜態檔案，例如 images, videos 等等
 	fileServer := http.FileServer(http.Dir("./static/"))
