e88831ca38d33f282c016bb7872e39b7cf468004
baconyao <baconyao@baconyaode-MacBook-Air.local>
2021-09-16 00:36:31 +0800

    78 Writing tests for the Forms package

--- internal/forms/form_test.go
+++ internal/forms/form_test.go
@@ -41,3 +41,89 @@ func TestForm_Required(t *testing.T) {
 		t.Error("shows does not have required fields when it does")
 	}
 }
+
+func TestForm_Has(t *testing.T) {
+	// 驗證沒有 field 的 form
+	postedData := url.Values{}
+	form := New(postedData)
+	if form.Has("yao") {
+		t.Error("form shows has field when it doesn't")
+	}
+
+	// 驗證有 field 的 form
+	postedData = url.Values{}
+	postedData.Add("yao", "hi")
+	postedData.Add("judy", "cute")
+
+	// r.PostForm = postedData
+	form = New(postedData)
+	if !form.Has("judy") || !form.Has("yao") {
+		t.Error("shows form doesn't have field when it should")
+	}
+}
+
+func TestForm_MinLength(t *testing.T) {
+	postedValues := url.Values{}
+	form := New(postedValues)
+	form.MinLength("yao", 3)
+
+	if form.Valid() {
+		t.Error("form shows min lenght for non-existent field")
+	}
+
+	isError := form.Errors.Get("yao")
+	if isError == "" {
+		t.Error("should have error but did not get one")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("some_field", "some value")
+	form = New(postedValues)
+	form.MinLength("some_field", 100)
+
+	if form.Valid() {
+		t.Error("shows minlength of 100 met when data is shorter")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("another_field", "abc123")
+	form = New(postedValues)
+	form.MinLength("another_field", 1)
+
+	if !form.Valid() {
+		t.Error("shows minlength of 1 is not met when it is")
+	}
+
+	isError = form.Errors.Get("another_field")
+	if isError != "" {
+		t.Error("should not have error but got one")
+	}
+}
+
+func TestForm_IsEmail(t *testing.T) {
+	postedValues := url.Values{}
+	form := New(postedValues)
+	form.IsEmail("aaa")
+
+	if form.Valid() {
+		t.Error("form shows valid email for non-existent field")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("email", "abc123@hi.com")
+	form = New(postedValues)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		t.Error("got an invalid email when we should not have")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("email", "lalala")
+	form = New(postedValues)
+	form.IsEmail("email")
+
+	if form.Valid() {
+		t.Error("got valid for invalid email address")
+	}
+}
